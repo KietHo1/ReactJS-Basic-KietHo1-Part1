a504453230cd64b5173fa4a95018e14049d7d30f
KietHo1 <kiethostartup1999@gmail.com>
2023-03-20 16:14:29 +0700

    video 22 React season 1 FULL

--- src/views/Nav/Nav.js
+++ src/views/Nav/Nav.js
@@ -1,25 +1,17 @@
-import React from 'react';
-import './Nav.scss';
-import {
-    Link, NavLink
-} from "react-router-dom";
+import React from "react";
+import "./Nav.scss";
 
 class Nav extends React.Component {
-    render() {
-        return (
-            <div className="topnav">
-                <NavLink to="/" activeClassName="active" exact={true}>
-                    Home
-                </NavLink>
-                <NavLink to="/todo" activeClassName="active">
-                    Todos
-                </NavLink>
-                <NavLink to="/about" activeClassName="active">
-                    About
-                </NavLink>
-
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="topnav">
+        <a className="active" href="/">
+          Home
+        </a>
+        <a href="/todo">Todos</a>
+        <a href="/about">About</a>
+      </div>
+    );
+  }
 }
 export default Nav;
