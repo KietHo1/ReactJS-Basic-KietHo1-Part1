35e5d8583b921adf5dd9f18353adedf4b79a7477
KietHo1 <kiethostartup1999@gmail.com>
2023-03-20 15:41:35 +0700

    video 19 React season 1

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+// import "./Demo.scss";
 class ChildComponent extends React.Component {
   //re-render
   state = {
@@ -26,7 +26,13 @@ class ChildComponent extends React.Component {
       <>
         {showJobs === false ? (
           <div>
-            <button onClick={() => this.handleShowHide()}>Show</button>
+            <button
+              style={{ color: "red" }}
+              className="btn-show"
+              onClick={() => this.handleShowHide()}
+            >
+              Show
+            </button>
           </div>
         ) : (
           <>
