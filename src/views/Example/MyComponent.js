6f454c9858b85daccbfe8bb374986bd8114fe733
KietHo1 <kiethostartup1999@gmail.com>
2023-03-20 15:34:19 +0700

    video 15 React season 1

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,6 +1,6 @@
 import React from "react";
+import AddComponent from "./AddComponent";
 import ChildComponent from "./ChildComponent";
-
 class MyComponent extends React.Component {
   //key:value
   state = {
@@ -18,46 +18,12 @@ class MyComponent extends React.Component {
     fragment
     */
 
-  handleChangeFirstName = (event) => {
-    this.setState({
-      firstName: event.target.value,
-    });
-  };
-  handleChangeLastName = (event) => {
-    this.setState({
-      lastName: event.target.value,
-    });
-  };
-  handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(">>> check data input: ", this.state);
-  };
   //re-render
   render() {
     console.log(">>> call render: ", this.state);
     return (
       <>
-        <form>
-          <label htmlFor="fname">First name:</label>
-          <br />
-          <input
-            type="text"
-            value={this.state.firstName}
-            onChange={(event) => this.handleChangeFirstName(event)}
-          />
-          <br />
-          <label htmlFor="lname">Last name:</label>
-          <br />
-          <input
-            type="text"
-            value={this.state.lastName}
-            onChange={(event) => this.handleChangeLastName(event)}
-          />
-          <br />
-          <br />
-          <input type="submit" onClick={(event) => this.handleSubmit(event)} />
-        </form>
-
+        <AddComponent />
         <ChildComponent
           name={this.state.firstName}
           age={"25"}
